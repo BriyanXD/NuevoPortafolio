0c08fa22c318197ea0c52f116320b4c7667dd7fa
Briyan A <briyana1997@gmail.com>
2022-12-08 16:00:22 -0400

    Contact SVG

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,6 +3,8 @@ import styles from "./Contact.module.css";
 import ThemeContext from "../../context/ThemeContext";
 import Linkedin from '../icons/linkedin';
 import GitHub from '../icons/GitHub';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faPaperPlane} from "@fortawesome/free-regular-svg-icons"
 
 const Contact = () => {
 
@@ -13,20 +15,28 @@ const Contact = () => {
                 <div className={styles.containerInfo}>
                 <h2 className={styles.title}>Contacto</h2>
                     <p className={styles.description}>Contactame para participar en tu equipo como desarrolador.</p>
-                    <p className={styles.email}>briyana1997@gmail.com</p>
+                    <a className={`${styles.email} ${styles[`email${theme}`]}`} href="">briyana1997@gmail.com</a>
                     <div className={styles.containerIcons}>
-                        <div className={styles.icon}><Linkedin fill={theme === 'dark'? '#fff': '#000'} /></div>
-                        <div className={styles.icon}><GitHub fill={theme === 'dark'? '#fff': '#000'} /></div>
+                        <div className={styles.icon}>
+                            <a href="https://www.linkedin.com/in/briyanbohtelo/" className={styles.icon} target='_BLANK'>
+                                <Linkedin fill={theme === 'dark'? '#fff': '#242424'} />
+                            </a>
+                        </div>
+                        <div className={styles.icon}>
+                            <a href="https://github.com/BriyanXD" target='_BLANK'>
+                                <GitHub fill={theme === 'dark'? '#fff': '#242424'}/>
+                            </a>
+                        </div>
                     </div>
                 </div>
                 <form className={styles.containerForm} action="">
                     <div className={styles.containerInput}>
                         <input className={`${styles.input} ${styles[`input${theme}`]}`} type="text" name="name" id="name" placeholder='Nombre'/>
-                        <input className={`${styles.input} ${styles[`input${theme}`]}`} type="text" name="email" id="email" placeholder='Correo'/>
+                        <input className={`${styles.input} ${styles[`input${theme}`]}`} type="input" name="email" id="email" placeholder='Correo/Telefono'/>
                     </div>
                     <textarea className={`${styles.input} ${styles[`input${theme}`]}`} name="message" id="message" placeholder='Mensaje' cols="30" rows="10"></textarea>
                     <div>
-                        <input className={`${styles.button} ${styles[`button${theme}`]}`} type="submit" value='Enviar' />
+                        <FontAwesomeIcon icon={faPaperPlane} className={`${styles.button} ${styles[`button${theme}`]}`} type="submit"/>
                     </div>
                 </form>
         </section>
